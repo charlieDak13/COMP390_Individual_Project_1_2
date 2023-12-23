74207cba76260c677d32aea493e0a3c07386c640
charlieDak13 <131418566+charlieDak13@users.noreply.github.com>
2023-12-23 18:16:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import meteor_data_class
 import util_functions
-from pathlib import Path
 
 #   meteorite_landing_data.txt
 
@@ -20,10 +18,8 @@ while True:
     # checks if user wants to break, otherwise continues
     if mode == "break":
         break
-        # mode_input = util_functions.final_mode
     # gives user the option of categories to filter by and the option to quit
     category_input = util_functions.check_category()
-
     if category_input == "1":
         # sets lower limit and checks if user wants to quit
         lower_limit_input = util_functions.check_lower_limit_mass()
@@ -41,33 +37,38 @@ while True:
                 split_list = line.strip('\n').split('\t')
                 util_functions.check_list_length(split_list)
                 meteor_object = util_functions.make_meteor_object(split_list)
-                
+
     # mass filter
                 if meteor_object.mass == '':
                     continue
                 elif float(lower_limit_input) < float(meteor_object.mass) < float(upper_limit_input):
-                    mass_list.append(meteor_object.name + ', ' + meteor_object.mass)
+                    mass_list.append(meteor_object.name + '\t' + meteor_object.id + '\t' + meteor_object.nameType
+                                     + '\t' + meteor_object.recClass + '\t' + meteor_object.mass + '\t'
+                                     + meteor_object.fall + '\t' + meteor_object.year + '\t' + meteor_object.recLat
+                                     + '\t' + meteor_object.recLong + '\t' + meteor_object.geoLocation + '\t'
+                                     + meteor_object.states + '\t' + meteor_object.countries)
         # prints tables and ends program after
         data_selection = util_functions.check_data_selection()
         if data_selection == "1":
-            util_functions.print_mass_table(mass_list)
+            util_functions.print_table(mass_list)
             break
         elif data_selection == "2":
             util_functions.write_to_text_file(util_functions.get_clean_datetime_string(),
                                               util_functions.list_to_tab_sep_string(mass_list))
             break
         elif data_selection == "3":
-            continue
+            util_functions.filtered_results_to_excel(mass_list)
+            break
         elif data_selection == "4":
             break
     elif category_input == '2':
         # sets lower limit and checks if user wants to quit
         low_year_input = util_functions.check_lower_year()
-        if util_functions.check_lower_year() == "break":
+        if low_year_input == "break":
             break
         # sets upper limit and checks if user wants to quit
         high_year_input = util_functions.check_upper_year()
-        if util_functions.check_upper_year() == "break":
+        if high_year_input == "break":
             break
         # runs program line for line with the upper and lower limits
         file = open(file, mode)
@@ -81,10 +82,26 @@ while True:
                 if meteor_object.year == '':
                     continue
                 elif int(high_year_input) > int(meteor_object.year) >= int(low_year_input):
-                    year_list.append(meteor_object.name + ',' + meteor_object.year)
+                    year_list.append(meteor_object.name + '\t' + meteor_object.id + '\t' + meteor_object.nameType
+                                     + '\t' + meteor_object.recClass + '\t' + meteor_object.mass + '\t'
+                                     + meteor_object.fall + '\t' + meteor_object.year + '\t' + meteor_object.recLat
+                                     + '\t' + meteor_object.recLong + '\t' + meteor_object.geoLocation + '\t'
+                                     + meteor_object.states + '\t' + meteor_object.countries)
         # prints year table and breaks after
-        util_functions.print_year_table(year_list)
-        break
+        data_selection = util_functions.check_data_selection()
+        if data_selection == "1":
+            util_functions.print_table(year_list)
+            break
+        elif data_selection == "2":
+            util_functions.write_to_text_file(util_functions.get_clean_datetime_string(),
+                                              util_functions.list_to_tab_sep_string(year_list))
+            break
+        elif data_selection == "3":
+            util_functions.filtered_results_to_excel(year_list)
+            break
+        elif data_selection == "4":
+            break
+
     elif category_input == '3':
         # ends the program
         print('Exiting the program. Goodbye!')
